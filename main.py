eaa0a9fcfcb1db1dd3245d4d23d25e489a27af4f
GutierrezLidiaSA <71468354+GutierrezLidiaSA@users.noreply.github.com>
2021-05-20 22:11:23 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ not analog pins :(
 '''
 # 36ish healthy human tempurature
 while True:
+    #3100 is for voltage input into the microbit. causes problems when battery is low
+    #1024 is the microvolt range of the TMP36
     raw = pins.analog_read_pin(AnalogPin.P0) * (3100 / 1024)
     #tempurature in celcius VVV
+    #500 is the 0 celsius point defined by TMP36
+    #10 is millivolt per degree
+    #56 is to calibrate it
     temp = ((raw - 500.0) / 10) - 56
 #    basic.show_number(temp_C)
 #    basic.pause(3000)
